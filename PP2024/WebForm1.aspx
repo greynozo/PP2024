c1f5764a21fe39355d55a3b85de7911b411d9fa2
greynozo <kioc04@hotmail.com>
2024-03-26 20:46:22 -0300

    Agregamos boton

--- PP2024/WebForm1.aspx
+++ PP2024/WebForm1.aspx
@@ -10,6 +10,8 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+
         </div>
     </form>
 </body>
--- PP2024/WebForm1.aspx.designer.cs
+++ PP2024/WebForm1.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace PP2024
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
